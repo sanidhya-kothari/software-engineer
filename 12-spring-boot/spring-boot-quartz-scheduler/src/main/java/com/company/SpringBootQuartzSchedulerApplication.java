f7f0fa17362e95045393be912a26149861e096b6
sanidhya-kothari <sankokothari@gmail.com>
2025-01-01 09:00:00 +0530

    learn spring boot quartz scheduler

---
+++ 12-spring-boot/spring-boot-quartz-scheduler/src/main/java/com/company/SpringBootQuartzSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuartzSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuartzSchedulerApplication.class, args);
+	}
+
+}
