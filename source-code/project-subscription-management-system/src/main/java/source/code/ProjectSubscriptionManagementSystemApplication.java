ddf1b61787b79e1c3234a6c5860834fbab6178d7
sanidhya-kothari <sankokothari@gmail.com>
2025-02-08 14:02:29 +0530

    create project structure for subscription management system

---
+++ source-code/project-subscription-management-system/src/main/java/source/code/ProjectSubscriptionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package source.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSubscriptionManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSubscriptionManagementSystemApplication.class, args);
+	}
+
+}
---
+++ source-code/project-subscription-management-system/src/test/java/source/code/ProjectSubscriptionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package source.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSubscriptionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
